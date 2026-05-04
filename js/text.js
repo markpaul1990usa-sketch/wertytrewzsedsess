7770c264b7b5d86cef332a416b7c96242d34d494
help <markpaul1990usa@gmail.com>
2026-05-04 10:43:40 -0700

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,1 @@
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography. Your Google Account has been disabled. Immediately call Online Security Support +1 (888) 821-4814to unlock it!';
